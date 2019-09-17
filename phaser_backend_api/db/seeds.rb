dd7d413f8e06ed43db19b4bb4d229b66f60fcfe1
Peter Dilenschneider <peterdilenschneider@Peters-MacBook-Pro-2.local>
2019-09-16 20:50:38 -0400

    set backend, migration, seed

---
+++ phaser_backend_api/db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+# scoreboard1 = Scoreboard.create()
+
+players = Player.create([
+    {name: "Ranger", time: 666, scoreboard_id: 1},
+    {name: "Thor", time: 999, scoreboard_id: 1}
+])
+
+# p "Created Scoreboard #{scoreboard1}"
+# p "Created Players #{players}"
